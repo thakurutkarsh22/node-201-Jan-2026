bbf3f32e32c3bcb7fa928c7fedcdcfd12f1b133f
Utkarsh Thakur <thakurutkarsh22@gmail.com>
2026-01-12 20:31:33 +0530

    session 4 precode

---
+++ Session4/Routes/ActivityRoute.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { getAllUsers, getUserByGender, getUsersByFirstName } = require('../Controller/ActivityUserController');
+const passwordAuthMiddleware = require('../Middleware/passwordAuthMiddleware');
+const router = express.Router();
+
+
+
+// 1. get all users 
+
+router.get("/getAllUsers", passwordAuthMiddleware, getAllUsers);
+
+
+// 2. get all the users by gender 
+// WAY 1: query PARAMS: 
+//  https://www.google.com/search?q=sehwaga   ? - query PARAMS.   (q is key, sehwaga is value)
+router.get("/getUsersByGender", passwordAuthMiddleware, getUserByGender);
+
+
+// 3. get user by first name
+// WAY 2: URL params  : -> means its url params 
+// https://pokeapi.co/api/v2/pokemon/pikachu | https://pokeapi.co/api/v2/pokemon/ditto 
+router.get("/getUsersByFirstName/:firstName", getUsersByFirstName);
+
+
+module.exports = router;
---
+++ Session4/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const ActivityRouter = require('./Routes/ActivityRoute');
+const HomeRouter = require('./Routes/HomeRoute');
+const PORT = 8089;
+
+const server = express();
+require('dotenv').config()
+
+
+// for home page ONLY get req is allowed
+// this is a HANDLER function - handles the incoming request and sends response
+server.use("/", HomeRouter)
+
+server.get("/fitness", (req, res, next) => {
+    const fitnessInfo = {
+        name: "John Doe express",
+        age: 30,
+        height: "5ft 10in",
+        hobbies: ["running", "cycling", "swimming"],
+        address: {
+            street: "123 Main St",
+            city: "Anytown",
+            state: "CA",
+            zip: "12345"
+        },
+        shouldSleepEightHours: true
+    };
+
+    // behind the scenes send is doing JSON.stringify for objects
+    // send is only for String 
+
+    // json behind the scenes sets the content-type to application/json
+    // json is also Stringifying the object
+    res.status(200).json(fitnessInfo);
+});
+
+// ACTIVITY 
+// we need to support new functionality 
+// use supports - GET, POST, PUT, DELETE, PATCH
+server.use("/api/v1/users/", ActivityRouter)
+
+
+server.listen(PORT, () => {
+  console.log(`Thumbs Up Server is listening on port EXPRESS ${PORT}`);
+});
